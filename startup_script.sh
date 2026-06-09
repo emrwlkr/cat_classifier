1aea42c8ad512c97b8a2e36caa1c5e059664bd05
Emma Walker <92034777+emrwlkr@users.noreply.github.com>
2026-06-09 17:56:55 +0100

    remove hardcoded file paths in startup script

--- startup_script.sh
+++ startup_script.sh
@@ -2,4 +2,4 @@
 module purge
 module load Python/3.11.3-GCCcore-12.3.0
 
-source /gpfs3/well/lindgren/users/onr287/cat_classifier/.venv/bin/activate
+source /gpfs3/well/{GROUP_NAME}/users/{USER_NAME}/cat_classifier/.venv/bin/activate
